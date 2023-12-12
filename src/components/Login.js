4b9e0d149e53483c66e9b2de2d0b805d3a4cb4e6
arsatis <angzhengyong@gmail.com>
2023-12-12 17:22:34 +0800

    feat: implement basic login function using Firestore

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,72 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { addDoc, collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
 
 function Login({ setAuthenticated }) {
-  const [email, setEmail] = useState('');
+  const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    if (email === '' && password === '') { // Simplified login logic, TODO: replace with actual authentication logic after integrating backend
+  // TODO: shift this logic for initializing Firestore into a React Context
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDC_OOByj0zCcJEgYraBNTo_ItEq_tYOlY',
+    authDomain: 'kcal-arsatis.firebaseapp.com',
+    projectId: 'kcal-arsatis',
+    storageBucket: 'kcal-arsatis.appspot.com',
+    messagingSenderId: '69806283894',
+    appId: '1:69806283894:web:c2443945966740e3e31357',
+    measurementId: 'G-0J39BNLT7C'
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const handleLogin = async () => {
+    if (user === '' || password === '') {
+      alert('Username or password field is empty');
+      return;
+    }
+
+    const usersRef = collection(db, 'users');
+    const q = query(usersRef, where('name', '==', user), where('password', '==', password)) // TODO: compare hashed passwords
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.size === 0) {
+      alert('User does not exist or password is incorrect.');
+      return;
+    }
+
+    setAuthenticated(true);
+    navigate('/kcal');
+  };
+
+  const handleSignup = async () => {
+    if (user === '' || password === '') {
+      alert('Username or password field is empty');
+      return;
+    }
+
+    // ensure that user does not already exist
+    const usersRef = collection(db, 'users');
+    const q = query(usersRef, where('name', '==', user));
+    const querySnapshot = await getDocs(q);
+    
+    if (querySnapshot.size !== 0) {
+      alert('User with the given username already exists.');
+      return;
+    }
+
+    try {
+      await addDoc(collection(db, 'users'), {
+        name: user,
+        password: password, // TODO: write password after hashing
+        salt: '' // TODO: add random salt
+      });
+      console.log('User', user, 'has logged in.');
       setAuthenticated(true);
       navigate('/kcal');
-    } else {
-      alert('Email or password is incorrect');
+    } catch (e) {
+      alert('Error adding document: ', e);
     }
   };
 
@@ -27,11 +82,11 @@ function Login({ setAuthenticated }) {
         <h2>Login</h2>
         <form>
           <div className='login-items'>
-            <label className='login-label'>Email:</label>
+            <label className='login-label'>User:</label>
             <input
-              type='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type='text'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
               onKeyUp={handleKeyUp}
             />
           </div>
@@ -44,7 +99,10 @@ function Login({ setAuthenticated }) {
               onKeyUp={handleKeyUp}
             />
           </div>
-          <button className='login-button' type='button' onClick={handleLogin}>Login</button>
+          <div className='login-buttons'>
+            <button className='login-button' type='button' onClick={handleLogin}>Log In</button>
+            <button className='login-button' type='button' onClick={handleSignup}>Sign Up</button>
+          </div>
         </form>
       </div>
     </div>
