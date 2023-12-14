8c52ee80bb669bf3f1b22a8c51455783dd45919f
arsatis <angzhengyong@gmail.com>
2023-12-14 11:19:13 +0800

    feat: sync existing events from Firestore

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
 import Calendar from './body/Calendar';
 import EventForm from './body/EventForm';
 import EventList from './body/EventList';
+import { UserContext } from '../providers/UserProvider';
 
 function Body({ isEventListVisible }) {
-  const [selectedDate, setSelectedDate] = useState(new Date());
   const [events, setEvents] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { db, user } = useContext(UserContext);
+
+  useEffect(() => {
+    async function updateEvents() {
+      const events = await getEvents(db, user);
+      setEvents(events);
+    }
+    updateEvents();
+  }, []);
+  console.log(events);
   
   const handleEventDelete = (eventId) => {
     const updatedEvents = events.filter((event) => event.id !== eventId);
@@ -32,6 +44,26 @@ function Body({ isEventListVisible }) {
       </div>
     </div>
   );
+}
+
+async function getEvents(db, user) {
+  const eventsRef = collection(db, 'events');
+  const q = query(eventsRef, where('user', '==', user));
+  const querySnapshot = await getDocs(q);
+
+  if (querySnapshot.size !== 0) {
+    return querySnapshot.docs[0].data().events;
+  }
+  try {
+    await addDoc(collection(db, 'events'), {
+      user: user,
+      events: []
+    });
+  } catch (e) {
+    alert('There was an error with during user creation. Please log in again.');
+    console.error('Error creating events for user: ', e);
+  }
+  return [];
 }
 
 export default Body;
